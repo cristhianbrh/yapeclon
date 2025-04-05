126f48ab26c6eadb09e61780b0ca124d772cce95
cristhianbrh <cristhianalexand@gmail.com>
2025-04-05 12:38:38 -0500

    feat(main): route append house

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,7 @@ import 'package:yapeclon/screens/auth/create_password_screen.dart';
 import 'package:yapeclon/screens/auth/login_screen.dart';
 import 'package:yapeclon/screens/auth/register_data_screen.dart';
 import 'package:yapeclon/screens/home/home_screen.dart';
+import 'package:yapeclon/screens/home/house_screen.dart';
 
 void main() {
   runApp(MyApp());
@@ -14,13 +15,14 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'YAPECLON',
-      initialRoute: '/login',
+      initialRoute: '/home',
       routes: {
         '/login': (context) => LoginScreen(),
         '/crear-cuenta': (context) => CreateAccountScreen(),
         '/crear-clave': (context) => CreatePasswordScreen(),
         '/registrar-datos': (context) => RegisterDataScreen(),
         '/home': (context) => HomeScreen(),
+        '/house': (context) => HouseScreen(),
       },
     );
   }
