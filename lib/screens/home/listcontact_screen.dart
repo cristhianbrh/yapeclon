b794b237b4064ba400a74f5eee3d8dd18cd3c340
cristhianbrh <cristhianalexand@gmail.com>
2025-04-29 18:06:20 -0500

    feat(listcontact_screen): list contact append

--- lib/screens/home/listcontact_screen.dart
+++ lib/screens/home/listcontact_screen.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:iconify_flutter/iconify_flutter.dart';
+import 'package:iconify_flutter/icons/gridicons.dart';
+import 'package:iconify_flutter/icons/material_symbols.dart';
+import 'package:iconify_flutter/icons/subway.dart';
 
 class ListcontactScreen extends StatelessWidget {
   const ListcontactScreen({super.key});
@@ -8,22 +12,187 @@ class ListcontactScreen extends StatelessWidget {
     return Scaffold(
       // appBar: AppBar(title: Text('Inicio')),
       body: SafeArea(
-        child: Container(
-          width: double.infinity,
-          height: double.infinity,
-          decoration: BoxDecoration(
-            gradient: LinearGradient(
-              begin: Alignment.topCenter,
-              end: Alignment.bottomCenter,
-              colors: [Color.fromARGB(255, 148, 102, 168), Color(0xFF720e9e)],
+        child: Column(
+          // Usar Column en lugar de SingleChildScrollView para evitar el error
+          children: [
+            _topHeader(),
+            _headerPageView(),
+            SizedBox(height: 20),
+            Container(
+              height: 60,
+              width: double.infinity,
+              margin: const EdgeInsets.all(8),
+              padding: const EdgeInsets.symmetric(horizontal: 12),
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(8),
+                boxShadow: [
+                  BoxShadow(
+                    color: Colors.black12,
+                    offset: Offset(0, 0),
+                    blurRadius: 3,
+                  ),
+                ],
+              ),
+              child: Row(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Iconify(Subway.search, color: Colors.black26),
+                  Expanded(
+                    child: TextField(
+                      keyboardType: TextInputType.text,
+                      decoration: InputDecoration(
+                        border: InputBorder.none,
+                        hintText: "Ingresa celular o busca contacto",
+                        hintStyle: TextStyle(
+                          color: Color.fromARGB(62, 34, 34, 17),
+                          fontSize: 15,
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            SizedBox(height: 20),
+            Expanded(
+              // Aquí hemos agregado Expanded para evitar el desbordamiento
+              child: SingleChildScrollView(
+                child: Container(
+                  padding: EdgeInsets.symmetric(horizontal: 25),
+                  child: Column(
+                    children: [
+                      _ContactView(),
+                      _ContactView(),
+                      _ContactView(),
+                      _ContactView(),
+                      _ContactView(),
+                      _ContactView(),
+                      _ContactView(),
+                      _ContactView(),
+                      _ContactView(),
+                      _ContactView(),
+                      _ContactView(),
+                      _ContactView(),
+                    ],
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _topHeader() {
+    return Container(
+      padding: EdgeInsets.symmetric(vertical: 15, horizontal: 15),
+      child: Row(
+        spacing: 20,
+        children: [
+          Iconify(
+            MaterialSymbols.close_rounded,
+            color: Colors.black45,
+            size: 30,
+          ),
+          Text(
+            "Yapear",
+            style: TextStyle(fontWeight: FontWeight.w500, fontSize: 20),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _headerPageView() {
+    return Container(
+      decoration: BoxDecoration(
+        color: Colors.white,
+        boxShadow: [
+          BoxShadow(
+            color: Colors.black12, // Sombra negra con opacidad
+            spreadRadius: -5, // Qué tanto se extiende
+            blurRadius: 5, // Qué tan difusa es la sombra
+            offset: Offset(0, 7), // Desplazamiento (x, y)
+          ),
+        ],
+      ),
+      child: Row(
+        children: [
+          Expanded(
+            child: Container(
+              height: 50,
+              decoration: BoxDecoration(
+                color: Colors.white,
+                border: Border(
+                  bottom: BorderSide(color: Colors.purple, width: 5),
+                ),
+              ),
+              child: Center(
+                child: Text(
+                  "Contactos",
+                  style: TextStyle(
+                    fontWeight: FontWeight.w500,
+                    fontSize: 16,
+                    color: Colors.purple,
+                  ),
+                  textAlign: TextAlign.center,
+                ),
+              ),
             ),
           ),
-          child: Column(
-            children: [
-              //CODIFICA AQUÍ
-            ],
+          Expanded(
+            child: Container(
+              height: 50,
+              decoration: BoxDecoration(
+                color: Colors.white,
+                border: Border(
+                  bottom: BorderSide(color: Colors.transparent, width: 5),
+                ),
+              ),
+              child: Center(
+                child: Text(
+                  "Yapeos pendientes",
+                  style: TextStyle(
+                    fontWeight: FontWeight.w500,
+                    fontSize: 16,
+                    color: Colors.black54,
+                  ),
+                  textAlign: TextAlign.center,
+                ),
+              ),
+            ),
           ),
-        ),
+        ],
+      ),
+    );
+  }
+
+  Widget _ContactView() {
+    return Container(
+      width: double.infinity,
+      decoration: BoxDecoration(
+        border: Border(bottom: BorderSide(color: Colors.black12, width: 1)),
+      ),
+      padding: const EdgeInsets.symmetric(vertical: 10),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text(
+            "100 Soles Probl",
+            style: TextStyle(fontWeight: FontWeight.w500, fontSize: 16),
+          ),
+          Text(
+            "955 274 602",
+            style: TextStyle(
+              fontSize: 16,
+              color: Colors.black38,
+              fontWeight: FontWeight.w400,
+            ),
+          ),
+          SizedBox(height: 10),
+        ],
       ),
     );
   }
